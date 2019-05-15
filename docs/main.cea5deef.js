bf698ca8986501b11a113f383846357d32e9501c
Michał Czapkowicz <czapkowicz@nonstop.co>
2019-05-15 19:55:59 +0200

    latest build

--- docs/main.cea5deef.js
+++ docs/main.cea5deef.js
@@ -333,7 +333,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59919" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60207" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
