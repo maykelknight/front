5ac30bc3478cec388f8b1834727b78b40477503d
Michał Czapkowicz <czapkowicz@nonstop.co>
2019-05-17 23:17:54 +0200

    latest build

--- docs/main.2b755954.js
+++ docs/main.2b755954.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60518" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50388" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -392,5 +392,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], "context")
 //# sourceMappingURL=/main.2b755954.js.map
