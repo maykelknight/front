610a54d0d866423b3fe3efff8900e8363f668e91
Michał Czapkowicz <czapkowicz@nonstop.co>
2019-05-19 16:47:28 +0200

    latest build

--- docs/main.cea5deef.js
+++ docs/main.cea5deef.js
@@ -323,8 +323,7 @@ function showNavigationMenuOnHamburgerClicked() {
   });
 }
 
-function hideLoader() {
-  document.querySelector('.preloader-wrapper').style.display = 'none';
+function hideLoader() {// document.querySelector('.preloader-wrapper').style.display = 'none';
 }
 },{"../scss/main.scss":"assets/scss/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -354,7 +353,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63791" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64069" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
